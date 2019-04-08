3a9a4cc9b0ead6abb0ce72294c3f909e411edd27
dixon2018 <dixon@randomsoftware.net>
2019-04-08 14:13:45 +0530

    Add Google Analytics

---
+++ js/google.js
@@ -0,0 +1,5 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-137909120-1"></script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-137909120-1');
