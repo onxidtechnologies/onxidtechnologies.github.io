6641f61896eae92a496cda4e6ffebebdc4b0ab0d
dixon2018 <dixon@randomsoftware.net>
2019-04-14 17:19:03 +0530

    Add spring boot tutorials

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+    $('.tutorial').hide();
+    $('#tutorial001').show();
+    $('.menu-btn').removeClass('btn-dark');
+    $('.menu-btn').addClass('btn-secondary');
+    $('#menu-btn001').removeClass('btn-secondary');
+    $('#menu-btn001').addClass('btn-dark');
+
     $(".menu-btn").click(function () {
         $('.menu-btn').removeClass('btn-dark');
         $('.menu-btn').addClass('btn-secondary');
